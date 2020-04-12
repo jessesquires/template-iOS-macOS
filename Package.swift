2811c9375fed088cb5dd4a95c8fba68ae1dc849b
Jesse Squires <jessesquires@users.noreply.github.com>
2020-04-12 09:53:13 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version 
+// of Swift required to build this package.
+// ----------------------------------------------------
+//
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//  Documentation
+//  https://jessesquires.github.io/PROJECT
+//
+//  GitHub
+//  https://github.com/jessesquires/PROJECT
+//
+//  Copyright © 2020-present Jesse Squires
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "jessesquires",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "PROJECT",
+            targets: ["PROJECT"])
+    ],
+    targets: [
+        .target(
+            name: "PROJECT",
+            path: "Sources"),
+        .testTarget(name: "PROJECT_TESTS",
+                    dependencies: ["PROJECT"],
+                    path: "Tests")
+    ],
+    swiftLanguageVersions: [.v5]
+)
