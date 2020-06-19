c32e0888dd47f4070b9aca14c9f10238f591fb2a
Jesse Squires <jessesquires@users.noreply.github.com>
2020-06-18 17:30:29 -0700

    update jazzy 0.13.4

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -9,7 +9,7 @@
 #  https://github.com/realm/jazzy/releases/latest
 #  ------------------------------
 
-VERSION="0.13.3"
+VERSION="0.13.4"
 
 FOUND=$(jazzy --version)
 LINK="https://github.com/realm/jazzy"
